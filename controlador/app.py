6805ecb72f42ad0c03a2384903ce1d336e593384
cecyRdz <92197205+cecyRdz@users.noreply.github.com>
2022-02-27 10:45:37 -0600

    Avance

--- controlador/app.py
+++ controlador/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_bootstrap import Bootstrap
+
 app = Flask(__name__, template_folder='../vista', static_folder='../static')
 Bootstrap(app)
+
+@app.route('/')
+def inicio():
+    return render_template('comunes/login.html')
+
+@app.route('/recopilarDatosLogin',methods=['post'])
+def validarUsuario():
+    return render_template('comunes/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
